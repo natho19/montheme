084930e7a5bf85604dfc4dae60fb8b22cf22561d
Nathan Akpawu <nakpawu@gmail.com>
2026-06-22 09:59:26 +0000

    first commit

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+
+function montheme_supports() {
+    add_theme_support('title-tag');
+}
+
+function montheme_register_assets() {
+    wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css', [], null);
+    wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js', [], null, true);
+    wp_register_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js', ['jquery', 'popper'], null, true);
+
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_script('bootstrap');
+}
+
+function montheme_title_separator() {
+    return '|';
+}
+
+add_action('after_setup_theme', 'montheme_supports');
+add_action('wp_enqueue_scripts', 'montheme_register_assets');
+add_filter('document_title_separator', 'montheme_title_separator');
